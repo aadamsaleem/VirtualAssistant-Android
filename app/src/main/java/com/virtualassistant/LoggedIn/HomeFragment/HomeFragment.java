df5a13343255c3d5da1f0eecf2da19592ad3607b
Aadam Saleem <aadam.saleem@gmail.com>
2017-04-12 19:50:08 -0400

    chat

---
+++ app/src/main/java/com/virtualassistant/LoggedIn/HomeFragment/HomeFragment.java
@@ -0,0 +1,192 @@
+package com.virtualassistant.LoggedIn.HomeFragment;
+
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import com.virtualassistant.R;
+import com.virtualassistant.models.ChatMessage;
+
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by aadam on 11/4/2017.
+ */
+
+public class HomeFragment extends android.support.v4.app.Fragment {
+
+    public static Chat chat;
+    public Bot bot;
+    private ListView mListView;
+    private FloatingActionButton mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
+    private ChatMessageAdapter mAdapter;
+    private View view;
+
+    public HomeFragment(){};
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        setupViews();
+
+        mAdapter = new ChatMessageAdapter(getContext(), new ArrayList<ChatMessage>());
+        mListView.setAdapter(mAdapter);
+
+        //code for sending the message
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = mEditTextMessage.getText().toString();
+                //bot
+                String response = chat.multisentenceRespond(mEditTextMessage.getText().toString());
+                if (TextUtils.isEmpty(message)) {
+                    return;
+                }
+                sendMessage(message);
+                mimicOtherMessage(response);
+                mEditTextMessage.setText("");
+                mListView.setSelection(mAdapter.getCount() - 1);
+            }
+        });
+        //checking SD card availablility
+        boolean a = isSDCARDAvailable();
+        Log.e("a",""+a);
+        //receiving the assets from the app directory
+        AssetManager assets = getResources().getAssets();
+        File jayDir = new File(Environment.getExternalStorageDirectory().toString() + "/aadam/bots/Aadam");
+        boolean b = jayDir.mkdirs();
+        Log.e("b",""+b);
+        if (jayDir.exists()) {
+            //Reading the file
+            try {
+                for (String dir : assets.list("Aadam")) {
+                    File subdir = new File(jayDir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file : assets.list("Aadam/" + dir)) {
+                        File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assets.open("Aadam/" + dir + "/" + file);
+                        out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
+                        //copy file from assets to the mobile's SD card or any secondary memory
+                        copyFile(in, out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        //get the working directory
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/aadam";
+        System.out.println("Working Directory = " + MagicStrings.root_path);
+        AIMLProcessor.extension =  new PCAIMLProcessorExtension();
+        //Assign the AIML files to bot for processing
+        bot = new Bot("Aadam", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String[] args = null;
+        mainFunction(args);
+
+        return view;
+    }
+
+    //region private methods
+    private void setupViews(){
+
+        getViewIds();
+
+
+    }
+
+    private void getViewIds(){
+        mListView = (ListView) view.findViewById(R.id.listView);
+        mButtonSend = (FloatingActionButton) view.findViewById(R.id.btn_send);
+        mEditTextMessage = (EditText) view.findViewById(R.id.et_message);
+        mImageView = (ImageView) view.findViewById(R.id.iv_image);
+    }
+
+    //check SD card availability
+    public static boolean isSDCARDAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ? true : false;
+    }
+
+    //Request and response of user and the bot
+    public static void mainFunction(String[] args) {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+        Timer timer = new Timer();
+        String request = "Hello.";
+        String response = chat.multisentenceRespond(request);
+
+        System.out.println("Human: " + request);
+        System.out.println("Robot: " + response);
+    }
+
+    private void sendMessage(String message) {
+        ChatMessage chatMessage = new ChatMessage(message, true, false);
+        mAdapter.add(chatMessage);
+
+        //mimicOtherMessage(message);
+    }
+
+    private void mimicOtherMessage(String message) {
+        ChatMessage chatMessage = new ChatMessage(message, false, false);
+        mAdapter.add(chatMessage);
+    }
+
+    private void sendMessage() {
+        ChatMessage chatMessage = new ChatMessage(null, true, true);
+        mAdapter.add(chatMessage);
+
+        mimicOtherMessage();
+    }
+    private void mimicOtherMessage() {
+        ChatMessage chatMessage = new ChatMessage(null, false, true);
+        mAdapter.add(chatMessage);
+    }
+
+    //copying the file
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+    //endregion
+
+}
--- app/src/main/java/com/virtualassistant/LoggedIn/MainActivity.java
+++ app/src/main/java/com/virtualassistant/LoggedIn/MainActivity.java
@@ -1,17 +1,57 @@
 package com.virtualassistant.LoggedIn;
 
+import android.Manifest;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
+import com.virtualassistant.LoggedIn.HomeFragment.ChatMessageAdapter;
+import com.virtualassistant.LoggedIn.HomeFragment.HomeFragment;
 import com.virtualassistant.R;
+import com.virtualassistant.models.ChatMessage;
+
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManagerFactory;
 
 public class MainActivity extends AppCompatActivity {
+
 
     private Fragment fragment;
     private FragmentManager fragmentManager;
@@ -23,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setupView();
+
+        setupCA();
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
 
         navigation.inflateMenu(R.menu.navigation);
         navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -45,6 +94,9 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+
+
     }
 
     private void setupView() {
@@ -58,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void getViewIds() {
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
     }
 
     private void addFragment(Fragment fragment) {
@@ -65,4 +118,38 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.content, fragment).commit();
     }
 
+    private void setupCA(){
+//        try {// Load CAs from an InputStream
+//// (could be from a resource or ByteArrayInputStream or ...)
+//            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+//// From https://www.washington.edu/itconnect/security/ca/load-der.crt
+//            InputStream caInput = new BufferedInputStream(new FileInputStream("load-der.crt"));
+//            Certificate ca;
+//            try {
+//                ca = cf.generateCertificate(caInput);
+//                System.out.println("ca=" + ((X509Certificate) ca).getSubjectDN());
+//            } finally {
+//                caInput.close();
+//            }
+//
+//// Create a KeyStore containing our trusted CAs
+//            String keyStoreType = KeyStore.getDefaultType();
+//            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+//            keyStore.load(null, null);
+//            keyStore.setCertificateEntry("ca", ca);
+//
+//// Create a TrustManager that trusts the CAs in our KeyStore
+//            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+//            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+//            tmf.init(keyStore);
+//
+//// Create an SSLContext that uses our TrustManager
+//            SSLContext context = SSLContext.getInstance("TLS");
+//            context.init(null, tmf.getTrustManagers(), null);
+//// Tell the okhttp to use a SocketFactory from our SSLContext
+//            OkHttpClient client = new OkHttpClient.Builder().sslSocketFactory(context.getSocketFactory()).build();
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+    }
 }
